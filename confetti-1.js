4e5fff2ca37d9fa9789315683ad5ed58fbf0ee9f
akaushal10 <49487500+akaushal10@users.noreply.github.com>
2020-09-09 22:58:44 +0530

    Add files via upload

---
+++ confetti-1.js
@@ -0,0 +1,213 @@
+var confettiLarge = {
+	maxCount: 150,		//set max confettiLarge count
+	speed: 2,			//set the particle animation speed
+	frameInterval: 15,	//the confettiLarge animation frame interval in milliseconds
+	alpha: 1.0,			//the alpha opacity of the confettiLarge (between 0 and 1, where 1 is opaque and 0 is invisible)
+	gradient: false,	//whether to use gradients for the confettiLarge particles
+	start: null,		//call to start confettiLarge animation (with optional timeout in milliseconds, and optional min and max random confettiLarge count)
+	stop: null,			//call to stop adding confettiLarge
+	toggle: null,		//call to start or stop the confettiLarge animation depending on whether it's already running
+	pause: null,		//call to freeze confettiLarge animation
+	resume: null,		//call to unfreeze confettiLarge animation
+	togglePause: null,	//call to toggle whether the confettiLarge animation is paused
+	remove: null,		//call to stop the confettiLarge animation and remove all confettiLarge immediately
+	isPaused: null,		//call and returns true or false depending on whether the confettiLarge animation is paused
+	isRunning: null		//call and returns true or false depending on whether the animation is running
+};
+
+(function() {
+	confettiLarge.start = startconfettiLarge;
+	confettiLarge.stop = stopconfettiLarge;
+	confettiLarge.toggle = toggleconfettiLarge;
+	confettiLarge.pause = pauseconfettiLarge;
+	confettiLarge.resume = resumeconfettiLarge;
+	confettiLarge.togglePause = toggleconfettiLargePause;
+	confettiLarge.isPaused = isconfettiLargePaused;
+	confettiLarge.remove = removeconfettiLarge;
+	confettiLarge.isRunning = isconfettiLargeRunning;
+	var supportsAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame;
+	var colors = ["rgba(30,144,255,", "rgba(107,142,35,", "rgba(255,215,0,", "rgba(255,192,203,", "rgba(106,90,205,", "rgba(173,216,230,", "rgba(238,130,238,", "rgba(152,251,152,", "rgba(70,130,180,", "rgba(244,164,96,", "rgba(210,105,30,", "rgba(220,20,60,"];
+	var streamingconfettiLarge = false;
+	var animationTimer = null;
+	var pause = false;
+	var lastFrameTime = Date.now();
+	var particles = [];
+	var waveAngle = 0;
+	var context = null;
+
+	function resetParticle(particle, width, height) {
+		particle.color = colors[(Math.random() * colors.length) | 0] + (confettiLarge.alpha + ")");
+		particle.color2 = colors[(Math.random() * colors.length) | 0] + (confettiLarge.alpha + ")");
+		particle.x = Math.random() * width;
+		particle.y = Math.random() * height - height;
+		particle.diameter = Math.random() * 10 + 5;
+		particle.tilt = Math.random() * 10 - 10;
+		particle.tiltAngleIncrement = Math.random() * 0.07 + 0.05;
+		particle.tiltAngle = Math.random() * Math.PI;
+		return particle;
+	}
+
+	function toggleconfettiLargePause() {
+		if (pause)
+			resumeconfettiLarge();
+		else
+			pauseconfettiLarge();
+	}
+
+	function isconfettiLargePaused() {
+		return pause;
+	}
+
+	function pauseconfettiLarge() {
+		pause = true;
+	}
+
+	function resumeconfettiLarge() {
+		pause = false;
+		runAnimation();
+	}
+
+	function runAnimation() {
+		if (pause)
+			return;
+		else if (particles.length === 0) {
+			context.clearRect(0, 0, window.innerWidth, window.innerHeight);
+			animationTimer = null;
+		} else {
+			var now = Date.now();
+			var delta = now - lastFrameTime;
+			if (!supportsAnimationFrame || delta > confettiLarge.frameInterval) {
+				context.clearRect(0, 0, window.innerWidth, window.innerHeight);
+				updateParticles();
+				drawParticles(context);
+				lastFrameTime = now - (delta % confettiLarge.frameInterval);
+			}
+			animationTimer = requestAnimationFrame(runAnimation);
+		}
+	}
+
+	function startconfettiLarge(timeout, min, max) {
+		var width = window.innerWidth;
+		var height = window.innerHeight;
+		window.requestAnimationFrame = (function() {
+			return window.requestAnimationFrame ||
+				window.webkitRequestAnimationFrame ||
+				window.mozRequestAnimationFrame ||
+				window.oRequestAnimationFrame ||
+				window.msRequestAnimationFrame ||
+				function (callback) {
+					return window.setTimeout(callback, confettiLarge.frameInterval);
+				};
+		})();
+		var canvas = document.getElementById("confettiLarge-canvas");
+		if (canvas === null) {
+			canvas = document.createElement("canvas");
+			canvas.setAttribute("id", "confettiLarge-canvas");
+			canvas.setAttribute("style", "display:block;z-index:999999;pointer-events:none;position:fixed;top:0");
+			document.body.prepend(canvas);
+			canvas.width = width;
+			canvas.height = height;
+			window.addEventListener("resize", function() {
+				canvas.width = window.innerWidth;
+				canvas.height = window.innerHeight;
+			}, true);
+			context = canvas.getContext("2d");
+		} else if (context === null)
+			context = canvas.getContext("2d");
+		var count = confettiLarge.maxCount;
+		if (min) {
+			if (max) {
+				if (min == max)
+					count = particles.length + max;
+				else {
+					if (min > max) {
+						var temp = min;
+						min = max;
+						max = temp;
+					}
+					count = particles.length + ((Math.random() * (max - min) + min) | 0);
+				}
+			} else
+				count = particles.length + min;
+		} else if (max)
+			count = particles.length + max;
+		while (particles.length < count)
+			particles.push(resetParticle({}, width, height));
+		streamingconfettiLarge = true;
+		pause = false;
+		runAnimation();
+		if (timeout) {
+			window.setTimeout(stopconfettiLarge, timeout);
+		}
+	}
+
+	function stopconfettiLarge() {
+		streamingconfettiLarge = false;
+	}
+
+	function removeconfettiLarge() {
+		stop();
+		pause = false;
+		particles = [];
+	}
+
+	function toggleconfettiLarge() {
+		if (streamingconfettiLarge)
+			stopconfettiLarge();
+		else
+			startconfettiLarge();
+	}
+	
+	function isconfettiLargeRunning() {
+		return streamingconfettiLarge;
+	}
+
+	function drawParticles(context) {
+		var particle;
+		var x, y, x2, y2;
+		for (var i = 0; i < particles.length; i++) {
+			particle = particles[i];
+			context.beginPath();
+			context.lineWidth = particle.diameter;
+			x2 = particle.x + particle.tilt;
+			x = x2 + particle.diameter / 2;
+			y2 = particle.y + particle.tilt + particle.diameter / 2;
+			if (confettiLarge.gradient) {
+				var gradient = context.createLinearGradient(x, particle.y, x2, y2);
+				gradient.addColorStop("0", particle.color);
+				gradient.addColorStop("1.0", particle.color2);
+				context.strokeStyle = gradient;
+			} else
+				context.strokeStyle = particle.color;
+			context.moveTo(x, particle.y);
+			context.lineTo(x2, y2);
+			context.stroke();
+		}
+	}
+
+	function updateParticles() {
+		var width = window.innerWidth;
+		var height = window.innerHeight;
+		var particle;
+		waveAngle += 0.01;
+		for (var i = 0; i < particles.length; i++) {
+			particle = particles[i];
+			if (!streamingconfettiLarge && particle.y < -15)
+				particle.y = height + 100;
+			else {
+				particle.tiltAngle += particle.tiltAngleIncrement;
+				particle.x += Math.sin(waveAngle) - 0.5;
+				particle.y += (Math.cos(waveAngle) + particle.diameter + confettiLarge.speed) * 0.5;
+				particle.tilt = Math.sin(particle.tiltAngle) * 15;
+			}
+			if (particle.x > width + 20 || particle.x < -20 || particle.y > height) {
+				if (streamingconfettiLarge && particles.length <= confettiLarge.maxCount)
+					resetParticle(particle, width, height);
+				else {
+					particles.splice(i, 1);
+					i--;
+				}
+			}
+		}
+	}
+})();
